f0ff65a804a6f73ae2bd0bb33df14ce764560708
Lydia kaze <lydiakaze6@gmail.com>
2025-06-17 17:35:57 +0200

    Created project repositories and files

---
+++ directory_creator.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+mkdir -p {controller,model,view,tests} && touch {main.py, requirements.txt} controller/app_controller.py model/downloader.py view/gui.py tests/test_downloader.py
+
+
